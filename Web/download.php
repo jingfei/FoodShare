1a97e6804cbb55e9ca011cf46f2b033e828aebec
jingfei <jingfei955047@gmail.com>
2015-10-24 17:40:48 +0800

    Change method for php call server. Delete go-pear.

--- Web/download.php
+++ Web/download.php
@@ -2,7 +2,18 @@
     $id = $_GET['pic_id'];
     $file_path = $_GET['file_path'];    //for testing only
     $url = "https://hackathon.promise.com.tw/fileop/v1/files" . $file_path;
-    $http = new HttpRequest($url, HttpRequest::METH_GET);
-    $http->addHeaders(array('X-Auth-Token'=>'ebceae03a92f4a3babb9c9ba9432cb4d','User-Agent'=>'I am Jordan'));
-    $message = $http->send;
+//    $http = new HttpRequest($url, HttpRequest::METH_GET);
+//    $http->addHeaders(array('X-Auth-Token'=>'ebceae03a92f4a3babb9c9ba9432cb4d','User-Agent'=>'I am Jordan'));
+//    $message = $http->send;
+	$options = array(
+    	'http' => array(
+	    	'header'  => "X-Auth-Token: ebceae03a92f4a3babb9c9ba9432cb4d\r\n" . 					 "User-Agent:I am Jordan\r\n",
+			'method'  => 'POST',
+//			'content' => http_build_query($data),
+		),
+	);
+	$context  = stream_context_create($options);
+	$result = file_get_contents($url, false, $context);
+
+	var_dump($result);
 ?>
