3aca53b532e8835f589ec335f094771d843882a0
jorrdansu1126 <newjordansu1126@gmail.com>
2015-10-24 20:56:26 +0800

    now you can upload pic

--- Web/download.php
+++ Web/download.php
@@ -1,8 +1,8 @@
 <?php
     header('Content-Type: image/jpeg;');
     $id = $_GET['pic_id'];
-    $file_path = $_GET['file_path'];    //for testing only
-    $url = "https://hackathon.promise.com.tw/fileop/v1/files" . $file_path;
+//  $file_path = $_GET['file_path'];    //for testing only
+    $url = "https://hackathon.promise.com.tw/fileop/v1/files/" . $id;
     //$url = "https://hackathon.promise.com.tw/fileop/v1/files/test.jpg";
 //    $http = new HttpRequest($url, HttpRequest::METH_GET);
 //    $http->addHeaders(array('X-Auth-Token'=>'ebceae03a92f4a3babb9c9ba9432cb4d','User-Agent'=>'I am Jordan'));
