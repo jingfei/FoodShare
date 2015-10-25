7c45beab61c6dcfd69227fd98a7d7145a80379c8
jingfei <jingfei955047@gmail.com>
2015-10-25 09:41:51 +0800

    Add upload description.

--- Web/upload.php
+++ Web/upload.php
@@ -1,3 +1,66 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Feed Me - list</title>
+		<meta charset="UTF-8" />
+		<script src="./js/jquery-1.11.3.min.js"></script>
+		<script src="./js/masonry.pkgd.min.js"></script>
+		<link rel="stylesheet" href="./css/search.css" type="text/css"/>
+		<style>
+		.grid{ margin:10px 180px; }
+		.grid-item {
+			width: 300px;
+			border:1px solid gray;
+			border-radius:20px;
+			margin-bottom:30px;
+			background: #ccf8ff;
+		}
+		.grid-item img{
+			border-radius: 20px;
+		}
+		.grid-item p{
+			padding:0 20px;
+		}
+		</style>
+		<link rel="stylesheet" href="./css/home.css" type="text/css"/>
+	</head>
+	<body style="margin:0;font-family:Microsoft JhengHei; background-image:url('./img/wood.jpg'); background-size:100% 100%;">
+	    <div id="navigation" class="no-scroll">
+		    <div id="navigation-wrap">
+		   		<div id="nav">
+					<ul id="nav-ul">
+						<li>
+							<a id="buttonH" href="index.php">HOME</a>
+						</li>
+						<li></li>
+						<li>
+							<a id="buttonI" href="map.php">地圖尋找</a>
+						</li>
+						<li>
+							<a id="buttonA" href="">列表尋找</a>
+						</li>
+						<li>
+							<a id="buttonL" href="post.php">分享美食</a>
+						</li>
+						<li>
+							<a id="buttonG" href="">我要評價</a>
+						</li>
+						<li>
+							<a id="buttonQ" href="">登入</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div style="position:fixed">
+			<img style="width:100px;position:fixed;left:70px;bottom:150px;" src="./img/bread.png" /><br/><br/>
+			<img style="width:100px;position:fixed;left:20px;bottom:50px;" src="./img/coffee.png" />
+			<img style="width:100px;position:fixed;right:70px;top:100px;" src="./img/pizza.png" />
+			<img style="width:100px;position:fixed;right:20px;top:200px;" src="./img/juice.png" />
+		</div>
+		<h1 style="margin:50px;color:white;">已分享您的美食，請靜候佳音</h1>
+	</body>
+</html>
 <?php
     // I want to get last pID first
     require_once "openDB.php";
@@ -16,7 +79,7 @@
     $uploadFile = file_get_contents($_FILES["pic"]["tmp_name"]);
     $add_sql = "INSERT INTO `foodshare`.`posts` (`pID`, `topic`, `content`, `eatPlace`, `address`, `price`, `expire_time`, `quantity`)
                 VALUES ('{$pID}', '{$topic}', '{$content}', '{$eatPlace}', '{$address}', '{$price}', '{$expire_time}', '{$quantity}');";
-    echo $add_sql;
+  //  echo $add_sql;
     mysqli_query($link, $add_sql);
 
     //mkdir for the post first
